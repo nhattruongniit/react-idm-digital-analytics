5c5c4108dbd3697e15187577f8c67e39e0ab4d6e
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2020-11-25 00:02:25 +0700

    done add topbar, navbar

--- src/modules/Dashboard/NavBar/NavBar.js
+++ src/modules/Dashboard/NavBar/NavBar.js
@@ -11,6 +11,8 @@ import FolderDetails20 from "@carbon/icons-react/lib/folder--details/20";
 import Settings20 from "@carbon/icons-react/lib/settings/20";
 import Help20 from "@carbon/icons-react/lib/help/20";
 import ChevronDown20 from "@carbon/icons-react/lib/chevron--down/20";
+import Search20 from "@carbon/icons-react/lib/search/20";
+import Snowflake20 from "@carbon/icons-react/lib/snowflake/20";
 
 // assets
 import LogoIcon from 'assets/images/logo.svg';
@@ -31,6 +33,12 @@ function NavBar() {
         <div className="navbar_options">
           <HeaderGlobalAction aria-label="Folder" onClick={() => {}}>
             <FolderDetails20 />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="Folder" onClick={() => {}}>
+            <Snowflake20 />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="Folder" onClick={() => {}}>
+            <Search20 />
           </HeaderGlobalAction>
           <HeaderGlobalAction aria-label="Settings" onClick={() => {}}>
             <Settings20 />
